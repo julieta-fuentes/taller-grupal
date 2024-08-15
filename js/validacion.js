f75bba5583e7b8642515a7a7bedcd659125e3a50
Zamirapereyra <zampereyra12@gmail.com>
2024-08-15 09:56:55 -0300

    Subir rama

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,17 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const password1 = document.getElementById('password1');
+    const password2 = document.getElementById('password2');
+
+    password2.addEventListener('input', () => {
+        const areEqual = password1.value === password2.value;
+        if (areEqual) {
+            console.log('Las contraseñas coinciden.');
+        } else {
+            console.log('Las contraseñas no coinciden.');
+        }
+    });
+});
